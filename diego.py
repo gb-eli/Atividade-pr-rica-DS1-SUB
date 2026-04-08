f4760d8b1e4aa2984b26261ffb5524d9cdfeff2f
diegofreitassantos-cpu <diego.freitas.santos@escola.pr.gov.br>
2026-04-07 22:20:52 -0300

    Add files via upload

---
+++ diego.py
@@ -0,0 +1,24 @@
+def menu_roupas():
+    print("menu")
+    print("1 - casaco")
+    print("2 - camisa polo")
+    print("3 - camisa regata ")
+    print("4 - sim")
+
+    roupa = (input("qual roupa voce gostaria"))
+
+    if roupa == "1":
+      print("voce escolheu casaco")
+      
+    elif roupa == "2":
+      print("voce escolheu camisa polo")
+
+    elif roupa == "3":
+      print("voce escolheu camisa regata")
+
+    elif roupa ==b"4":
+      print("sair")
+
+      
+
+menu_roupas()
